1dc54b0417ef95daf9bace53aef7c3434ee71ba3
Narendra <narendra.sunkara83@gmail.com>
2026-05-20 16:14:06 +0400

    sample programs

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "enter the user name"
+read USER_NAME
+
+echo "your user name is $USER_NAME"
+
+echo "enter your password"
+read -s PASSWORD
+
+echo "your password is $PASSWORD"
