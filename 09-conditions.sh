195b5c481021373aff1df3fb279f8049ecbff57c
Narendra <narendra.sunkara83@gmail.com>
2026-05-20 16:47:13 +0400

    sample programs

---
+++ 09-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 20 ]; then
+   echo "Given number : $NUMBER greater then 20"
+elif  [ $NUMBER -eq 20 ]; then
+   echo "Given number : $NUMBER equal to 20"
+
+else 
+   echo "Given number : $NUMBER lessthen 20"
