627c443879e69ef58b90daf98835cfb339720a57
Narendra <narendra.sunkara83@gmail.com>
2026-05-21 17:19:58 +0400

    sample programs

---
+++ 14-loops.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell_scripting"
+LOG_FILE="/var/log/shell_scriping/$0.log"
+
+if [ $USERID -ne 0 ]; then 
+   echo " Please run the script in Root User " | tee -a $LOG_FILE
+   exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo " $2 ....... FAILURE" | tee -a $LOG_FILE
+      exit 1
+    else
+      echo " $2 ...... SUCESS"  | tee -a $LOG_FILE
+    fi
+
+}
+
+for package in $@
+do
+ 
+   dnf list installed $package &>>$LOG_FILE
+
+   if [ $? -ne 0 ]; then
+      echo " $package not installed "  | tee -a $LOG_FILE
+      dnf install $package -y
+      VALIDATE $? " Installing $package"
+
+    else 
+      echo "$package already installed"  | tee -a $LOG_FILE
+    fi
+done
