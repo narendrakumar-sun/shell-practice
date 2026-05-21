33fa17f044bda33fccc96d7acea7d8b2826821e1
Narendra <narendra.sunkara83@gmail.com>
2026-05-21 13:27:05 +0400

    sample programs

---
+++ 11-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+   echo "please run the script in Root user"
+   exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+       echo "$2 ..... FAILURE"
+    else
+       echo "$2 ..... SUCESS"
+    fi
+}
+
+dnf install nginx -y 
+VALIDATE $? "installing nginx"
+
+dnf install mysql -y
+VALIDATE $? "installing mysl"
+
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
